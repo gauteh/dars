414da861799c557de0fa10bd6c53a65f4d927135
Gaute Hope <eg@gaute.vetsj.com>
2019-11-26 10:09:49 +0100

    String + Float -> full das for coads

--- src/nc.rs
+++ src/nc.rs
@@ -11,12 +11,6 @@ use async_std::task;
 
 use super::Dataset;
 
-fn ok<S>(x: S) -> Result<String, std::io::Error>
-    where S: Into<String>
-{
-    Ok::<_, std::io::Error>(x.into())
-}
-
 struct NcDas {
     das: Arc<String>
 }
@@ -26,26 +20,37 @@ impl NcDas {
         use netcdf::attribute::AttrValue::*;
 
         match a.value() {
-            Ok(Str(s)) => format!("{}String {} \"{}\"\n", " ".repeat(indent), a.name(), s), // TODO: escape
+            Ok(Str(s)) =>   format!("{}String {} \"{}\";\n", " ".repeat(indent), a.name(), s), // TODO: escape
+            Ok(Float(f)) => format!("{}Float32 {} {:+E};\n", " ".repeat(indent), a.name(), f),
 
             _ => "".to_string()
         }
     }
 
+    fn push_attr<'a>(indent: usize, das: &mut String, a: impl Iterator<Item = &'a netcdf::Attribute>) -> () {
+        das.push_str(&a.map(|aa| NcDas::format_attr(indent, aa)).collect::<String>());
+    }
+
     pub fn build(f: String) -> anyhow::Result<NcDas> {
-        debug!("opening: {} to build das", f);
+        debug!("building Data Attribute Structure (DAS) for {}", f);
 
         let nc = netcdf::open(f)?;
 
-        /* building */
         let indent = 4;
         let mut das: String = "Attributes {\n".to_string();
 
         if let Some(_) = nc.attributes().next() {
             das.push_str("    NC_GLOBAL {\n");
-            das.push_str(&nc.attributes().map(|a| NcDas::format_attr(2*indent, a)).collect::<String>());
+            NcDas::push_attr(2*indent, &mut das, nc.attributes());
             das.push_str("    }\n");
         }
+
+        for var in nc.variables() {
+            das.push_str(&format!("    {} {{\n", var.name()));
+            NcDas::push_attr(2*indent, &mut das, var.attributes());
+            das.push_str("    }\n");
+        }
+
         das.push_str("}");
 
         Ok(NcDas {
@@ -87,8 +92,6 @@ impl Dataset for NcDataset {
     }
 
     async fn das(&self) -> Result<Response<Body>, hyper::http::Error> {
-        debug!("building Data Attribute Structure (DAS)");
-
         let a = self.das.das.clone();
 
         Response::builder().body(Body::from(a.to_string()))

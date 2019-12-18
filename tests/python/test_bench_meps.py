7b709b1d0afcd378b37509f0e2b1df27b169a6f8
Gaute Hope <eg@gaute.vetsj.com>
2019-12-18 09:04:01 +0100

    test: meps name

--- tests/python/test_bench_meps.py
+++ tests/python/test_bench_meps.py
@@ -3,14 +3,21 @@ from netCDF4 import Dataset
 import numpy as np
 from . import *
 
-def test_dars_testData_z_sfc(dars, benchmark):
+def test_file_meps(dars, benchmark):
+  def k():
+    d = Dataset("data/meps_det_vc_2_5km_latest.nc")
+    sst = d.variables['x_wind_ml'][1:1000]
+
+  benchmark(k)
+
+def test_dars_meps(dars, benchmark):
   def k():
     d = Dataset(dars + "meps_det_vc_2_5km_latest.nc")
     sst = d.variables['x_wind_ml'][1:1000]
 
   benchmark(k)
 
-def test_thredds_testData_z_sfc(tds, benchmark):
+def test_thredds_meps(tds, benchmark):
   def k():
     d = Dataset(tds + "meps_det_vc_2_5km_latest.nc")
     sst = d.variables['x_wind_ml'][1:1000]

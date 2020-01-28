4d918315a8704f180173a098221177699024933d
Gaute Hope <eg@gaute.vetsj.com>
2020-01-28 10:34:38 +0100

    init: start hdf5 index

---
+++ hdfidx/src/chunk.rs
@@ -0,0 +1,4 @@
+pub struct Chunk {
+    pub size: u64,
+    pub offset: u64
+}
---
+++ hdfidx/src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate anyhow;
+
+pub mod idx;
+pub mod chunk;
+pub mod dataset;
+
+
+#[cfg(test)]
+mod tests {
+}

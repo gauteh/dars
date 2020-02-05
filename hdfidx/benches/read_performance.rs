a39a4195e463e6c6c2919a555606e307da1c1936
Gaute Hope <eg@gaute.vetsj.com>
2020-02-05 15:44:06 +0100

    bench: chunk + cont

--- hdfidx/benches/read_performance.rs
+++ hdfidx/benches/read_performance.rs
@@ -20,3 +20,34 @@ fn read_2d_chunked_nat(b: &mut Bencher) {
     b.iter(|| d.read_raw::<f32>().unwrap())
 }
 
+#[bench]
+fn read_t_float32_idx(b: &mut Bencher) {
+    let i = Index::index("tests/data/t_float.h5").unwrap();
+    let r = DatasetReader::with_dataset(i.dataset("d32_1").unwrap(), i.path()).unwrap();
+
+    b.iter(|| r.values::<f32>(None, None).unwrap())
+}
+
+#[bench]
+fn read_t_float32_nat(b: &mut Bencher) {
+    let h = hdf5::File::open("tests/data/t_float.h5").unwrap();
+    let d = h.dataset("d32_1").unwrap();
+
+    b.iter(|| d.read_raw::<f32>().unwrap())
+}
+
+#[bench]
+fn read_chunked_1d_idx(b: &mut Bencher) {
+    let i = Index::index("tests/data/chunked_oneD.h5").unwrap();
+    let r = DatasetReader::with_dataset(i.dataset("d_4_chunks").unwrap(), i.path()).unwrap();
+
+    b.iter(|| r.values::<f32>(None, None).unwrap())
+}
+
+#[bench]
+fn read_chunked_1d_nat(b: &mut Bencher) {
+    let h = hdf5::File::open("tests/data/chunked_oneD.h5").unwrap();
+    let d = h.dataset("d_4_chunks").unwrap();
+
+    b.iter(|| d.read_raw::<f32>().unwrap())
+}

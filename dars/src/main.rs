a703cb04030f25ea2c4739e49038bc73e0a5e3ae
Gaute Hope <eg@gaute.vetsj.com>
2020-06-26 09:11:42 +0200

    warp: log requests

--- dars/src/main.rs
+++ dars/src/main.rs
@@ -12,6 +12,7 @@ extern crate anyhow;
 
 use colored::Colorize;
 use env_logger::Env;
+use warp::Filter;
 
 mod data;
 mod hdf5;
@@ -31,9 +32,15 @@ async fn main() -> anyhow::Result<()> {
             "../data/coads_climatology.nc4",
         )?)),
     );
+    data.datasets.insert(
+        "meps_det_vc_2_5km_latest.nc".to_string(),
+        Arc::new(data::DatasetType::HDF5(hdf5::Hdf5Dataset::open(
+            "../data/meps_det_vc_2_5km_latest.nc",
+        )?)),
+    );
 
     let data = Arc::new(data);
-    let dars = data::filters::datasets(data);
+    let dars = data::filters::datasets(data).with(warp::log("dars::api"));
 
     info!("Listening on {}", "127.0.0.1:8001".yellow());
     warp::serve(dars).run(([127, 0, 0, 1], 8001)).await;

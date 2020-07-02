e2e01dcfc3acd0f303bfd8267d825b97f0d7bd34
Gaute Hope <eg@gaute.vetsj.com>
2020-07-02 12:39:17 +0200

    use nc4

--- tests/python/test_bench_meta.py
+++ tests/python/test_bench_meta.py
@@ -5,28 +5,28 @@ from . import *
 
 def test_file_metadata_z_sfc(data, benchmark):
   def k():
-    d = Dataset(data + "coads_climatology.nc")
+    d = Dataset(data + "coads_climatology.nc4")
     sst = d.variables['SST']
 
   benchmark(k)
 
 def test_dars_metadata_z_sfc(dars, benchmark):
   def k():
-    d = Dataset(dars + "coads_climatology.nc")
+    d = Dataset(dars + "coads_climatology.nc4")
     sst = d.variables['SST']
 
   benchmark(k)
 
 def test_thredds_metadata_z_sfc(tds, benchmark):
   def k():
-    d = Dataset(tds + "coads_climatology.nc")
+    d = Dataset(tds + "coads_climatology.nc4")
     sst = d.variables['SST']
 
   benchmark(k)
 
 def test_hyrax_metadata_z_sfc(hyrax, benchmark):
   def k():
-    d = Dataset(hyrax + "coads_climatology.nc")
+    d = Dataset(hyrax + "coads_climatology.nc4")
     sst = d.variables['SST']
 
   benchmark(k)
--- tests/python/test_bench_testData.py
+++ tests/python/test_bench_testData.py
@@ -5,28 +5,28 @@ from . import *
 
 def test_file_testData_z_sfc(data, benchmark):
   def k():
-    d = Dataset(data + "coads_climatology.nc")
+    d = Dataset(data + "coads_climatology.nc4")
     sst = d.variables['SST'][:]
 
   benchmark(k)
 
 def test_dars_testData_z_sfc(dars, benchmark):
   def k():
-    d = Dataset(dars + "coads_climatology.nc")
+    d = Dataset(dars + "coads_climatology.nc4")
     sst = d.variables['SST'][:]
 
   benchmark(k)
 
 def test_thredds_testData_z_sfc(tds, benchmark):
   def k():
-    d = Dataset(tds + "coads_climatology.nc")
+    d = Dataset(tds + "coads_climatology.nc4")
     sst = d.variables['SST'][:]
 
   benchmark(k)
 
 def test_hyrax_testData_z_sfc(hyrax, benchmark):
   def k():
-    d = Dataset(hyrax + "coads_climatology.nc")
+    d = Dataset(hyrax + "coads_climatology.nc4")
     sst = d.variables['SST'][:]
 
   benchmark(k)
